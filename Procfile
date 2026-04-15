c44707a4ef4dc056e9a77b07b763e4f59384fbc4
Parshva Shah <parshvajshahbhuj@gmail.com>
2026-04-16 01:05:41 +0530

    fix: run migrate and seed in Procfile start command

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
+web: python manage.py migrate && python manage.py seed_data && gunicorn core.wsgi:application
--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# Render build script
 set -o errexit
-
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py seed_data
